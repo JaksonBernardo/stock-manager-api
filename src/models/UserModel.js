8e64b835f971d22da29aaa37957de69ec8f74be6
Jakson <jaksondevweb@gmail.com>
2025-06-03 21:35:11 -0300

    CRIANDO MODEL DE USERS

---
+++ src/models/UserModel.js
@@ -0,0 +1,53 @@
+import db from '../db/Connection'
+
+const UserModel = {
+
+    create: async (name, email, password, department, role, companyId) => {
+
+        let query = "INSERT INTO users (name, email, password, department, role, company_id) VALUES (?, ?, ?, ?, ?, ?)"
+
+        const [result] = await db.execute(query, [name, email, password, department, role, companyId])
+
+        return
+
+    },
+    update: async (userId, name, email, password, department, role, companyId) => {
+
+        let query = "UPDATE users SET name = ?, email = ?, password = ?, department = ?, role = ? WHERE id = ? AND company_id = ?"
+
+        const [result] = await db.execute(query, [name, email, password, department, role, userId, companyId])
+
+        return
+
+    },
+    delete: async (userId, companyId) => {
+
+        let query = "DELETE FROM users WHERE id = ? AND company_id = ?"
+
+        const [result] = await db.execute(query, [userId, companyId])
+
+        return
+
+    },
+    findById: async (userId, companyId) => {
+
+        let query = "SELECT * FROM users WHERE id = ? AND company_id = ?"
+
+        const [rows] = await db.execute(query, [userId, companyId])
+
+        return rows[0]
+
+    },
+    findAll: async (companyId) => {
+
+        let query = "SELECT * FROM users WHERE company_id = ?"
+
+        const [rows] = await db.execute(query, [companyId])
+
+        return rows[0]
+
+    }
+
+}
+
+export default UserModel;
