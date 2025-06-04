52e9499ed984a7f52e068a979939049c2e2ed2f3
Jakson <jaksondevweb@gmail.com>
2025-06-03 21:06:29 -0300

    CRIANDO O MODEL DE ROLEPERMISSIONS

---
+++ src/models/RolePermissionModel.js
@@ -0,0 +1,35 @@
+import db from '../db/Connection'
+
+const RolePermissionModel = {
+
+    findByRole: async (roleId) => {
+
+        let query = "SELECT id_role, id_permission FROM role_permissions WHERE id_role = ?"
+
+        const [rows] = await db.execute(query, [roleId])
+
+        return rows[0]
+
+    },
+    create: async (roleId, permissionId) => {
+
+        let query = "INSERT INTO role_permissions (id_role, id_permission) VALUES (?, ?)"
+
+        const [result] = await db.execute(query, [roleId, permissionId])
+
+        return
+
+    },
+    delete: async (roleId, permissionId) => {
+
+        let query = "DELETE FROM role_permissions WHERE id_role = ? AND id_permission = ?"
+
+        const [result] = await db.execute(query, [roleId, permissionId])
+
+        return
+
+    }
+
+}
+
+export default RolePermissionModel;
